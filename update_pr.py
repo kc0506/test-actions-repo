f596ca6f9ff6a54f515945f2ecf2674b9c2c4bf8
kc0506 <89458301+kc0506@users.noreply.github.com>
2024-03-06 20:11:48 +0800

    Production (#5)
    * Update
    
    * Update
    
    * Update
    
    * Add echo-1
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

---
+++ update_pr.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+
+from github import Auth, Github
+
+token_path = Path(__file__).parent / ".github/token"
+token = os.getenv("TOKEN")
+if not token:
+    token = token_path.open().read()
+pr_number = int(os.getenv("PR", "5"))
+action = os.getenv("ACTION", "")
+version = os.getenv("VERSION", "")
+
+auth = Auth.Token(token)
+g = Github(auth=auth)
+repo = g.get_repo("ntu-grade-viewer/test-actions-repo")
+pr = repo.get_pull(pr_number)
+
+
+def section(title: str, body: list[str]):
+    return f"\n## {title}\n" + "\n".join(["- " + s for s in body])
+
+
+match action:
+    case "title":
+        pr.edit(title="")
+        pr.update()
+        print(version)
+    case "body":
+        body = (pr.body or "") + section(
+            "Commit messages (auto-generated)",
+            [commit.commit.message for commit in pr.get_commits()],
+        )
+
+        pr.edit(body=body)
+        pr.update()
+
+g.close()
